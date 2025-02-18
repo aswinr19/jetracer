48b8a28401c423ca91fcaf7478850f9b7d8a22f1
Arno Joosen <arno.joosen@outlook.com>
2025-02-18 21:53:05 +0100

    cleanup jetracer controller launch file

--- launch/jetracer_controllers.launch.py
+++ launch/jetracer_controllers.launch.py
@@ -1,22 +1,15 @@
 from launch import LaunchDescription
 from ament_index_python import get_package_share_directory
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, SetEnvironmentVariable, RegisterEventHandler, LogInfo
-from launch.event_handlers import OnProcessExit, OnProcessStart
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
-
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-
-
     robot_controllers = PathJoinSubstitution([
         get_package_share_directory("jetracer_description"),
             "config",
-            PythonExpression([
-                "'", "jetracer", "_controller.yaml", "'"
-            ])
+            "jetracer_controller.yaml"
     ])
 
     joint_state_broadcaster_spawner = Node(
@@ -35,10 +28,10 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-    joint_state_broadcaster_spawner,
-    RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[ackermann_steering_controller_spawner, LogInfo(msg='End joint_state_broadcaster_spawner')],
-        )
-    ),])
+        joint_state_broadcaster_spawner,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[ackermann_steering_controller_spawner]
+            ))        
+    ])
