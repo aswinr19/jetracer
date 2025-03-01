ff043ec80c024dafc16f83c25a3cf1b66e8b255f
Ce1ikE <enniscelik@gmail.com>
2025-03-01 17:29:37 +0100

    removed modified files after dev merge

--- launch/jetracer_description.launch.py
+++ launch/jetracer_description.launch.py
@@ -1,31 +1,19 @@
-# required to get the Xacro files
 import os
 import xacro
-# main libraries used in our ".launch" files to provide the base classes
-# LaunchDescription , Node
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
-# in order to retrieve the "/share" directory of a given package
-# e.g.: a package in "/home/user/ros_ws/install" using this function with
-#       argument "jetracer_bringup" will return
-#       "/home/user/ros_ws/install/share/jetracer_bringup/"
+
 from ament_index_python.packages import get_package_share_directory
 
-# explained in later packages...
-# but for now, it just allows us to pass arguments to this Launch file/script
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration , PythonExpression , Command
 
-
 def generate_launch_description():
-
-    # explained in later packages...
-    # but for now,
-    # - LaunchConfiguration : enables us to acquire a launch argument called 'use_sim_time'
-    # - DeclareLaunchArgument : enables us to pass arguments from above launch file or console
+    
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time',
+        name='use_sim_time',
         default_value='false',
         description='[ARG] tells the robot_state_publisher to use the simulation time or just unix timestamp'
     )
@@ -38,13 +26,11 @@ def generate_launch_description():
 
 
     main_xacro_file = os.path.join(
-        get_package_share_directory(
-            'jetracer_bringup'), 'urdf', 'jetracer_main.xacro'
+        get_package_share_directory('jetracer_description'),'urdf','jetracer_main.xacro'
     )
 
-    # of course we have to check if we actually have a file
     if os.path.exists(main_xacro_file):
-        jetank_description = Command(
+        jetracer_description = Command(
             # spaces are important here since this is if we entered it on the CLI
             ["xacro"," ",main_xacro_file," ","namespace:=",robot_namespace]
         )
@@ -52,9 +38,6 @@ def generate_launch_description():
         print(f'failed to open {main_xacro_file}')
         exit()
 
-    # finally return a LaunchDescription with the configured nodes
-    # NOTE: here we CONFIGURE the node NOT CREATE them.
-    # they were created in their respective packages
     return LaunchDescription([
         use_sim_time_arg,
         robot_namespace_arg,
@@ -64,10 +47,7 @@ def generate_launch_description():
             output='screen',
             namespace=robot_namespace,
             parameters=[{
-                # parameters are very important for other programs such as RViz or ros_gz_bridge
-                # to read the correct data they will use these exact names published by this Node
                 'robot_description': jetracer_description,
-                # this parameter will be usefull later on
                 'use_sim_time': use_sim_time
             }],
             remappings=[
@@ -76,3 +56,5 @@ def generate_launch_description():
             ]
         ),   
     ])
+        
+
