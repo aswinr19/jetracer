65737901aead7a8ad04c1f0a64caaf6ee0955675
Ce1ikE <enniscelik@gmail.com>
2025-03-01 17:28:58 +0100

    changed the jetracer package ready for namespaces

--- launch/jetracer_controllers.launch.py
+++ launch/jetracer_controllers.launch.py
@@ -4,32 +4,52 @@ from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration , PythonExpression
 
 def generate_launch_description():
     robot_controllers = PathJoinSubstitution([
         get_package_share_directory("jetracer_description"),
             "config",
-            "jetracer_controller.yaml"
+            "jetracer_controllers.yaml"
     ])
+
+    robot_namespace = LaunchConfiguration('ns')
+    robot_namespace_arg = DeclareLaunchArgument(
+        name='ns',
+        description='[ARG] required namespace to keep nodes and topics separate when running multiple robots in the same simulation'
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_sim_time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='[ARG] tells the robot_state_publisher to use the simulation time or just unix timestamp'
+    )
+
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['joint_state_broadcaster'],
+        namespace=robot_namespace,
     )
 
     ackermann_steering_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['ackermann_steering_controller',
-                   '--param-file',
-                   robot_controllers,
-                   '--controller-ros-args',
-                   '-r /ackermann_steering_controller/tf_odometry:=/tf',
-                   ],
+                   '--param-file',robot_controllers,
+        ],
+        namespace=robot_namespace,
+        remappings=[
+            ("/ackermann_steering_controller/tf_odometry","tf")
+        ]
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
+        robot_namespace_arg,
         joint_state_broadcaster_spawner,
         RegisterEventHandler(
             event_handler=OnProcessExit(
--- launch/jetracer_description.launch.py
+++ launch/jetracer_description.launch.py
@@ -14,7 +14,7 @@ from ament_index_python.packages import get_package_share_directory
 # explained in later packages...
 # but for now, it just allows us to pass arguments to this Launch file/script
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration , PythonExpression , Command
 
 
 def generate_launch_description():
@@ -30,10 +30,13 @@ def generate_launch_description():
         description='[ARG] tells the robot_state_publisher to use the simulation time or just unix timestamp'
     )
 
-    # there is always 1 xacro file that includes all other xacro files
-    # so we only need to find that one and ROS's xacro tool will take care
-    # of the rest for us. This main xacro file is located (in our situation at least)
-    # in the URDF directory
+    robot_namespace = LaunchConfiguration('ns')
+    robot_namespace_arg = DeclareLaunchArgument(
+        name='ns',
+        description='[ARG] required namespace to keep nodes and topics separate when running multiple robots in the same simulation'
+    )
+
+
     main_xacro_file = os.path.join(
         get_package_share_directory(
             'jetracer_bringup'), 'urdf', 'jetracer_main.xacro'
@@ -41,7 +44,10 @@ def generate_launch_description():
 
     # of course we have to check if we actually have a file
     if os.path.exists(main_xacro_file):
-        jetracer_description = xacro.process_file(main_xacro_file).toxml()
+        jetank_description = Command(
+            # spaces are important here since this is if we entered it on the CLI
+            ["xacro"," ",main_xacro_file," ","namespace:=",robot_namespace]
+        )
     else:
         print(f'failed to open {main_xacro_file}')
         exit()
@@ -51,17 +57,22 @@ def generate_launch_description():
     # they were created in their respective packages
     return LaunchDescription([
         use_sim_time_arg,
-        # first node node launches the "robot_state_publisher"
+        robot_namespace_arg,
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
+            namespace=robot_namespace,
             parameters=[{
                 # parameters are very important for other programs such as RViz or ros_gz_bridge
                 # to read the correct data they will use these exact names published by this Node
                 'robot_description': jetracer_description,
                 # this parameter will be usefull later on
                 'use_sim_time': use_sim_time
-            }]
-        )
+            }],
+            remappings=[
+                ('/tf','tf'),
+                ('/tf_static','tf_static'),
+            ]
+        ),   
     ])
