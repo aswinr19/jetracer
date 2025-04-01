66b75d65491bbe6bcc53c82a3107aaf2b5175e9c
Arno Joosen <arno.joosen@outlook.com>
2025-04-01 15:46:08 +0200

    fix slam toolbox rvize tool

--- jetracer_bringup/launch/rvize.launch.py
+++ jetracer_bringup/launch/rvize.launch.py
@@ -33,7 +33,22 @@ def generate_launch_description():
                 ('/camera/camera_info', 'camera/camera_info'),
                 ('/camera/image_raw/compressed', 'camera/image_raw/compressed'),
                 ('/camera/image_raw/compressedDepth', 'camera/image_raw/compressedDepth'),
-                ('/camera/image_raw/theora', 'camera/image_raw/theora')
+                ('/camera/image_raw/theora', 'camera/image_raw/theora'),
+                ('/slam_toolbox/clear_changes', 'slam_toolbox/clear_changes'),
+                ('/slam_toolbox/describe_parameters', 'slam_toolbox/describe_parameters'),
+                ('/slam_toolbox/deserialize_map', 'slam_toolbox/deserialize_map'),
+                ('/slam_toolbox/dynamic_map', 'slam_toolbox/dynamic_map'),
+                ('/slam_toolbox/get_interactive_markers', 'slam_toolbox/get_interactive_markers'),
+                ('/slam_toolbox/get_parameter_types', 'slam_toolbox/get_parameter_types'),
+                ('/slam_toolbox/get_parameters', 'slam_toolbox/get_parameters'),
+                ('/slam_toolbox/list_parameters', 'slam_toolbox/list_parameters'),
+                ('/slam_toolbox/manual_loop_closure', 'slam_toolbox/manual_loop_closure'),
+                ('/slam_toolbox/pause_new_measurements', 'slam_toolbox/pause_new_measurements'),
+                ('/slam_toolbox/save_map', 'slam_toolbox/save_map'),
+                ('/slam_toolbox/serialize_map', 'slam_toolbox/serialize_map'),
+                ('/slam_toolbox/set_parameters', 'slam_toolbox/set_parameters'),
+                ('/slam_toolbox/set_parameters_atomically', 'slam_toolbox/set_parameters_atomically'),
+                ('/slam_toolbox/toggle_interactive_mode', 'slam_toolbox/toggle_interactive_mode')
                 ],
             output='screen',
             arguments=[
